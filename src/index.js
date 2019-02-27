ef700f26f9c569f7593dc193b303bf8c905c5a93
Torkhau <torkhau@gmail.com>
2019-02-27 16:23:46 +0300

    First solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
+
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  
+  if (str.length%2 !== 0) {
+    return false;
+  };
+  let i = 0;
+  let lenBracketsConfig = bracketsConfig.length - 1;
+  while (i <= lenBracketsConfig) {
+    let target = getBracketsTarget(bracketsConfig[i]);
+    i++;
+    let index = str.indexOf(target, 0);
+    while (index >=0 ) {
+      str = str.slice(0, index) + str.slice(index + 2);
+      index = str.indexOf(target, 0);
+      i = 0;
+    };
+  };
+  return str.length == 0 ? true : false;
+};
+
+function getBracketsTarget(bracketsType) {
+  
+  let res = "";
+  for (let index = 0; index < bracketsType.length; index++) {
+    res += bracketsType[index]; 
+  }
+  return res;
+};
